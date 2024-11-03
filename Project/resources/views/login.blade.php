b1a6503b0a4ec32e24a6ec821c5787d1d2972376
Sebastian Guevara <yungdiablox@gmail.com>
2024-11-03 17:16:03 -0600

    Cambios

--- Project/resources/views/login.blade.php
+++ Project/resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11 | Posts</title>
+    <title>SPORTIVO</title>
     
     <style>
 
