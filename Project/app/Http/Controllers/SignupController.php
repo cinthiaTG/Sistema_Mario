8bda13db149a39b6aedb83b6ec7d0fd0a4803ea3
cinthiaTG <2230282@upv.edu.mx>
2024-11-06 11:21:26 -0600

    add algo

--- Project/app/Http/Controllers/SignupController.php
+++ Project/app/Http/Controllers/SignupController.php
@@ -9,4 +9,7 @@ class SignupController extends Controller{
     public function index(){
         return view("modulos.signup.index");
     }
+    public function store(){
+        //aqui va a guardar el nuevo perfil y me falta otro que valide que no exista ese perfil pero no me acuerdo como se hace eso
+    }
 }
