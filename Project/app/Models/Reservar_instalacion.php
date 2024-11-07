ffbaf75cccaa1782d0a1e3da4c10102793c19434
ZwTz <jdangro4@gmail.com>
2024-11-07 14:15:28 -0700

    prueba 1

---
+++ Project/app/Models/Reservar_instalacion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservar_instalacion extends Model
+{
+    use HasFactory;
+}
---
+++ Project/app/Models/Resultados_partido.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resultados_partido extends Model
+{
+    use HasFactory;
+}
