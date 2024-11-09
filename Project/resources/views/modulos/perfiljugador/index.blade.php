16d943cd93d14aadc05fd28781589e6dab73c976
Sebastian Guevara <yungdiablox@gmail.com>
2024-11-08 23:36:41 -0600

    Cambios

--- Project/resources/views/modulos/perfiljugador/index.blade.php
+++ Project/resources/views/modulos/perfiljugador/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Torneo</title>
-    <link rel="stylesheet" href="{{ asset('css/creartorneo.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('css/perfiljugador.css') }}"> 
 </head>
 <body>
     <div class="container">
@@ -18,54 +18,19 @@
             <img class="icon4" src="{{ asset('img/search.png') }}" alt="search">
         </div>
 
-        <div class="form-title">Crear Torneo</div>
+        <div class="form-title">Perfil Jugador</div>
         <form class="tournament-form">
-            <label>Subir Logotipo</label>
-            <input type="file" class="file-input">
-
-            <label>Nombre del torneo</label>
-            <input type="text">
-
-            <label>Categoría</label>
-            <input type="text">
-
-            <label>Tipo de Torneo</label>
-            <input type="text">
-
-            <label>Deporte</label>
-            <input type="text">
-
-            <div class="section-title">Patrocinadores</div>
-            <label>Nombre del patrocinador</label>
-            <input type="text">
-
-            <label>Monto del patrocinador</label>
-            <input type="text">
-
-            <div class="section-title">Precios</div>
-            <label>Costo del Torneo</label>
-            <input type="text">
-
-            <label>Costo de Inscripción</label>
-            <input type="text">
-
-            <div class="section-title">Equipos</div>
-            <label>Equipos a jugar</label>
-            <input type="text">
-
-            <div class="section-title">Fechas</div>
-            <label>Fecha inicial del Torneo</label>
-            <input type="date">
-
-            <label>Hora</label>
-            <input type="time">
-
-            <div class="section-title">Ubicación</div>
-            <label>Estado</label>
-            <input type="text">
-
-            <label>Ciudad</label>
-            <input type="text">
+            <img class="photo" src="{{ asset('img/photo.png') }}" alt="more">
+            <ul class="profile-details">
+                <li>Nombre:</li>
+                <li>Fecha de nacimiento:</li>
+                <li>Género:</li>
+                <li>Teléfono:</li>
+                <li>Edad:</li>
+                <li>Correo electrónico:</li>
+                <li>Rol (jugador, entrenador, árbitro, fanático):</li>
+            </ul>
+            
 
             <button type="submit" class="save-button">Guardar</button>
         </form>
