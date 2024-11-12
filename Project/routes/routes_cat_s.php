887b7d148ab1c4d8f3f33702a320504b63d938f0
Sebastian Guevara <yungdiablox@gmail.com>
2024-11-11 22:30:46 -0600

    Cambios

--- Project/routes/routes_cat_s.php
+++ Project/routes/routes_cat_s.php
@@ -3,26 +3,16 @@
 use App\Http\Controllers\CrearTorneoController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
-<<<<<<< HEAD
 use App\Http\Controllers\PerfilJugadorController;
 use App\Http\Controllers\Perfil2Controller;
 use App\Http\Controllers\PerfilController;
-=======
-use App\Http\Controllers\PerfilController;
-use App\Http\Controllers\PerfilJugadorController;
-use App\Http\Controllers\Perfil2Controller;
->>>>>>> f54c841e05a34a9b416707ceabf70b6122008b72
 use App\Http\Controllers\RegistrarJugadorController;
 use App\Http\Controllers\RegistrarResultadoUnoController;
 use App\Http\Controllers\RegistrarResultadoDosController;
 use App\Http\Controllers\RegistrarEquipoController;
 use App\Http\Controllers\EditarJugadorController;
 use App\Http\Controllers\EditarJugadorDosController;
-use App\Http\Controllers\EditarEquipoController;
-use App\Http\Controllers\EditarEquipoDosController;
 use App\Http\Controllers\SignupController;
-use App\Http\Controllers\EditarJugadorController;
-use App\Http\Controllers\EditarJugadorDosController;
 use App\Http\Controllers\EditarEquipoController;
 use App\Http\Controllers\EditarEquipoDosController;
 use Illuminate\Support\Facades\Route;
