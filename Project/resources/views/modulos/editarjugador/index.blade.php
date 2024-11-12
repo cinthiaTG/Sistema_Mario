0cc8a65ceb717b57fbb6b11129994301f7881e22
ZwTz <jdangro4@gmail.com>
2024-11-12 16:55:19 -0600

    CRUD Jugador limpio 1/3

--- Project/resources/views/modulos/editarjugador/index.blade.php
+++ Project/resources/views/modulos/editarjugador/index.blade.php
@@ -24,19 +24,16 @@
             <table class="results-table">
                 <thead>
                     <tr>
-                        <th>ID</th>
                         <th>Nombre</th>
                         <th>Apellido Paterno</th>
                         <th>Apellido Materno</th>
                         <th>Edad</th>
                         <th>Posición</th>
-                        <th>ID_Deporte</th>
-                        <th>ID_Equipo</th>
-                        <th>Puntos</th>
+                        {{-- <th>Puntos</th>
                         <th>Asistencias</th>
                         <th>Tarjetas Amarillas</th>
                         <th>Tarjetas Rojas</th>
-                        <th>Faltas</th>
+                        <th>Faltas</th> --}}
                         <th>Acciones</th>
                     </tr>
                 </thead>
@@ -48,11 +45,11 @@
                         <td>{{ $jugador->apellido_materno }}</td>
                         <td>{{ $jugador->edad }}</td>
                         <td>{{ $jugador->posicion }}</td>
-                        <td>{{ $jugador->puntos }}</td>
+                        {{-- <td>{{ $jugador->puntos }}</td>
                         <td>{{ $jugador->asistencias }}</td>
                         <td>{{ $jugador->tarjetas_amarillas }}</td>
                         <td>{{ $jugador->tarjetas_rojas }}</td>
-                        <td>{{ $jugador->faltas }}</td>
+                        <td>{{ $jugador->faltas }}</td> --}}
                         <td>
                             <a href="{{ route('jugadores.edit', $jugador->id) }}"><button class="edit-button">Editar</button></a>
                             <form action="{{ route('jugadores.destroy', $jugador->id) }}" method="POST" style="display:inline;">
